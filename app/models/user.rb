4b36f4bb531b1f4ac61de1dfe1ac75afe4166f94
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-07-26 13:01:28 +0000

    capistrano

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, :confirmable
+         :recoverable, :rememberable, :validatable
          
          has_many :user_bookings, dependent: :destroy
          has_many :bookings, through: :user_bookings, inverse_of: :users
