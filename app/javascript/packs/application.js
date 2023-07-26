ddc272198c60443796235ef500cf6b7592f8e955
AbdoulKamiss <abdoulayekamissoko20@gmail.com>
2023-07-26 08:40:24 +0000

    fix geolocation

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,24 +15,48 @@ require("jquery")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 function initMap() {
-    const latitude = parseFloat(document.getElementById('latitude').value)
-    const longitude = parseFloat(document.getElementById('longitude').value)
-    const latitude2 = parseFloat(document.getElementById('latitude2').value)
-    const longitude2 = parseFloat(document.getElementById('longitude2').value)
-    const locations = [
-        { lat: latitude, lng: longitude }, 
-        { lat: latitude2, lng: longitude2 }   
-      ];
-    const map = new google.maps.Map(document.getElementById("map"), {
-      zoom: 6,
-      center: locations[0],
-    });
-    for (const location of locations ) {
+  const latitude = parseFloat(document.getElementById('latitude').value);
+  const longitude = parseFloat(document.getElementById('longitude').value);
+  const latitude2 = parseFloat(document.getElementById('latitude2').value);
+  const longitude2 = parseFloat(document.getElementById('longitude2').value);
+  const locations = [
+    { lat: latitude, lng: longitude },
+    { lat: latitude2, lng: longitude2 }
+  ];
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 6,
+    center: locations[0],
+  });
+
+  const directionsService = new google.maps.DirectionsService();
+  const directionsRenderer = new google.maps.DirectionsRenderer({
+    map: map,
+  });
+
+  const origin = new google.maps.LatLng(latitude, longitude);
+  const destination = new google.maps.LatLng(latitude2, longitude2);
+
+  const request = {
+    origin: origin,
+    destination: destination,
+    travelMode: google.maps.TravelMode.DRIVING,
+  };
+
+  directionsService.route(request, (result, status) => {
+    if (status == google.maps.DirectionsStatus.OK) {
+      directionsRenderer.setDirections(result);
+    }
+  });
+
+  for (const location of locations) {
     const marker = new google.maps.Marker({
       position: location,
       map: map,
     });
-    }
-  };
-  window.initMap = initMap;
+  }
+}
+
+window.initMap = initMap;
+
   
